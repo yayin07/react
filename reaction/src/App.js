55fbe49d630556127e3d231ddfc92f7cbe9edc8a
clarence cabiles <clarencecabiles07@gmail.com>
2022-09-23 18:55:25 +0800

    Initial Commit

---
+++ reaction/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return;
+  <body>
+    <nav class="bg-red-500">hi</nav>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>;<div></div>
+  </body>;
+}
+
+export default App;
